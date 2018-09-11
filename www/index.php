0bc9e30d6ef6ea6c6050f3024f743ac08a05ba84
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-09-11 12:57:14 +0300

    change index.php

--- www/index.php
+++ www/index.php
@@ -1,3 +1,31 @@
 <?php declare(strict_types = 1);
 
-echo 'Hello! My name is Alexander Dankovtsev' . PHP_EOL;
+define('ROOT_PATH', dirname(__DIR__));
+
+require_once ROOT_PATH . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+$container = new DI\CachedContainer();
+
+$app = new Beauty\App($container);
+
+$request = new Beauty\Request(
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_SERVER,
+    $_FILES
+);
+
+try {
+    $app->process($request)->reply();
+} catch (Beauty\Exception\NotFound $e) {
+        var_dump($e->getMessage(), $e->getFile(), $e->getLine());
+    (new Controller\Errors($request))
+        ->notFound()
+        ->reply();
+} catch (Throwable $e) {
+    var_dump($e->getMessage(), $e->getFile(), $e->getLine(), $e->getCode());
+    (new Controller\Errors($request))
+        ->internalError()
+        ->reply();
+}
