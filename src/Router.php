a3ea3993ea5651160365f8570f3350749274bf55
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-09-15 11:24:38 +0300

    routes from cache

--- src/Router.php
+++ src/Router.php
@@ -5,4 +5,16 @@
  */
 class Router extends \Beauty\Router
 {
+    protected $routes;
+
+    /**
+     * @inheritdoc
+     */
+    public function getRoutes(): array
+    {
+        if (!$this->routes) {
+            $this->routes = include(ROOT_PATH . '/var/cache/routes/Router.php');
+        }
+        return $this->routes;
+    }
 }
