8a1f4e571a966af1e48a1d44bfc810f77f68eb57
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-03-09 12:51:07 +0300

    add sub router for admin

---
+++ src/Admin/Router.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace Admin;
+
+/**
+ * @router etc/routes/admin.yml
+ */
+class Router extends \Beauty\Router
+{
+    protected $routes;
+
+    /**
+     * @inheritdoc
+     */
+    public function getRoutes(): array
+    {
+        if (!$this->routes) {
+            $this->routes = include(ROOT_PATH . '/var/cache/routes/Admin/Admin/Router.php');
+        }
+        return $this->routes;
+    }
+}
