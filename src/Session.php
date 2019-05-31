122e5c07dfb4e94dbb8231b29b4c053011a12b97
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-05-31 09:22:38 +0300

    update session

--- src/Session.php
+++ src/Session.php
@@ -2,6 +2,8 @@
 
 class Session
 {
+    private const SESSION_ID_COOKIE_NAME = 's';
+
     /**
      * @var \Beauty\Request
      */
@@ -12,11 +14,64 @@ class Session
      */
     private $sessionClient;
 
+    /**
+     * @var string|null
+     */
+    private $id;
+
+    /**
+     * @var Session\Data|null
+     */
+    private $data;
+
     public function __construct(
         \Beauty\Request $request,
         \Session\SessionServiceClient $sessionClient
     ) {
         $this->request = $request;
         $this->sessionClient = $sessionClient;
+    }
+
+    public function getData(): Session\Data
+    {
+        if ($this->data === null) {
+            $sessionId = $this->getId();
+            $call = $this->sessionClient->Get(
+                (new \Session\GetRequest())
+                    ->setSessid($sessionId)
+            );
+            $response = $call->wait();
+            // todo: validate
+            $result = $response[0];
+            /** @var \Session\GetResponse $result */
+            $this->data = new Session\Data(
+                $sessionId,
+                $result->getUserId(),
+                $result->getAccessToken(),
+                $result->getRefreshToken()
+            );
+        }
+        
+        return $this->data;
+    }
+
+    /**
+     * @return string
+     */
+    private function getId(): string
+    {
+        if ($this->id === null) {
+            $call = $this->sessionClient->Create(
+                new \Session\CreateRequest()
+            );
+            $response = $call->wait();
+            // todo: validate
+            $result = $response[0];
+
+            /** @var \Session\CreateResponse $result */
+            $this->id = $result->getSessid();
+        }
+
+        return $this->id;
     }
 }
