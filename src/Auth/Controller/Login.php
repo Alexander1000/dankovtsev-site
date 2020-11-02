051530dc65b2771233ecf1988af7d780ab8286c7
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-02 13:10:44 +0300

    add login page

--- src/Auth/Controller/Login.php
+++ src/Auth/Controller/Login.php
@@ -11,6 +11,9 @@ class Login extends ControllerAbstract
      */
     public function loginAction(): Beauty\Http\ResponseInterface
     {
+        if ($this->request->isPost()) {
+            var_dump($this->request->getParams());
+        }
         return $this->render('auth/login');
     }
 }
