e7bf7bf6a73e1b1a8bb5411aad0db853c0bc012e
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-02 14:04:28 +0300

    authorize

--- src/Auth/Controller/Login.php
+++ src/Auth/Controller/Login.php
@@ -3,16 +3,38 @@
 namespace Auth\Controller;
 
 use Beauty;
+use Beauty\Request;
+use Alexander1000\Clients\Users;
+use Alexander1000\Clients\Auth;
 
 class Login extends ControllerAbstract
 {
+    private Users\Client $userClient;
+
+    private Auth\Client $authClient;
+
+    public function __construct(Request $request, Users\Client $userClient, Auth\Client $authClient)
+    {
+        parent::__construct($request);
+        $this->userClient = $userClient;
+        $this->authClient = $authClient;
+    }
+
     /**
      * @return Beauty\Http\ResponseInterface
      */
     public function loginAction(): Beauty\Http\ResponseInterface
     {
         if ($this->request->isPost()) {
-            var_dump($this->request->getParams());
+            $user = $this->userClient->getByEmail($this->request->getParam('email'));
+
+            $reqAuthenticate = new Auth\Request\V1\Authenticate(
+                new Auth\Model\Credential($user->getEmails()[0]->getId(), 'email'),
+                $this->request->getParam('password')
+            );
+            $token = $this->authClient->authenticate($reqAuthenticate);
+
+            // @todo: save in session
         }
         return $this->render('auth/login');
     }
