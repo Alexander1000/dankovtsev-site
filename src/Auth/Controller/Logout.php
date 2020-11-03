1a6fbe44ebdfb01fcb9d6b296efc65db0c02b862
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-03 11:23:58 +0300

    implement logout

--- src/Auth/Controller/Logout.php
+++ src/Auth/Controller/Logout.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Auth\Controller;
 
 use Beauty;
+use Alexander1000\Clients\Auth;
 
 class Logout extends ControllerAbstract
 {
@@ -18,6 +19,14 @@ class Logout extends ControllerAbstract
             return $this->redirect('/', 302);
         }
 
-        return $this->render('index');
+        $reqAuthLogout = new Auth\Request\V1\Logout($sessData->getAccessToken());
+        $this->authClient->logout($reqAuthLogout);
+
+        $sessData->setUserId(0);
+        $sessData->setAccessToken('');
+        $sessData->setRefreshToken('');
+        $this->session->save($sessData);
+
+        return $this->redirect('/', 302);
     }
 }
