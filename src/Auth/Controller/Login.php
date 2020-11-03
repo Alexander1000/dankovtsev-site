79b590c64a8211a9ed7ed60ac28a0a33c1211190
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-11-03 13:58:31 +0300

    confirm user on first success auth

--- src/Auth/Controller/Login.php
+++ src/Auth/Controller/Login.php
@@ -4,6 +4,7 @@ namespace Auth\Controller;
 
 use Beauty;
 use Alexander1000\Clients\Auth;
+use Alexander1000\Clients\Users;
 
 class Login extends ControllerAbstract
 {
@@ -21,6 +22,10 @@ class Login extends ControllerAbstract
             );
             $token = $this->authClient->authenticate($reqAuthenticate);
 
+            if ($user->getStatusId() == 0) {
+                $this->confirmUser($user);
+            }
+
             $sessData = $this->session->getData();
             $sessData
                 ->setUserId($user->getUserId())
@@ -32,5 +37,40 @@ class Login extends ControllerAbstract
             return $this->redirect('/', 302);
         }
         return $this->render('auth/login');
+    }
+    
+    private function confirmUser(Users\Response\V1\User $user)
+    {
+        $reqEmails = [];
+        if (count($user->getEmails()) > 0) {
+            foreach ($user->getEmails() as $email) {
+                $reqEmails[] = new Users\Request\V1\Save\Email(
+                    $email->getId(),
+                    $email->getStatusId(),
+                    $email->getEmail()
+                );
+            }
+        }
+
+        $reqPhones = [];
+        if (count($user->getPhones()) > 0) {
+            foreach ($user->getPhones() as $phone) {
+                $reqPhones[] = new Users\Request\V1\Save\Phone(
+                    $phone->getId(),
+                    $phone->getStatusId(),
+                    $phone->getPhone()
+                );
+            }
+        }
+
+        $reqUserUpdate = new Users\Request\V1\Save\User(
+            $user->getUserId(),
+            1,
+            $user->getLogin(),
+            !empty($reqEmails) ? $reqEmails : null,
+            !empty($reqPhones) ? $reqPhones : null
+        );
+
+        $this->userClient->save($reqUserUpdate);
     }
 }
